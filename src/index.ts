d2cf62127b0c10dc4725f25c92a392fab0352216
Igor Kuzmenko <kuzzmi@gmail.com>
2015-11-04 17:40:59 +0100

    Added Folder class

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,27 @@
+/// <reference path="../typings/node/node.d.ts" />
+
+var fs = require('fs');
+
 class FileClass {
-    fullpath: string;
-    
-    constructor(fullpath: string) {
-        this.fullpath = fullpath;
+    constructor(public fullpath: string) {
     }
 }
+
+class Folder {
+    files:  Array<FileClass> = [];
+
+    constructor(public path:string, public name:string, files: Array<string>) {
+        for(var index in files) {
+            this.files.push(new FileClass(files[index]));
+        }
+    }
+}
+
+fs.readdir('.', function(err, data) {
+    var folder = new Folder(__dirname, 'monk', data);
+
+    console.log(folder);
+});
 
 var file1 = new FileClass('testpath');
 console.log(file1.fullpath);
