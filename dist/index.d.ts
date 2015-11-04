f33529d6e9ff021b15017510b7d7fb212b57c6a0
Igor Kuzmenko <kuzzmi@gmail.com>
2015-11-04 16:10:19 +0100

    Removed dist folder from .git

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare class FileClass {
-    fullpath: string;
-    constructor(fullpath: string);
-}
-declare var file1: FileClass;
--- dist/index.js
+++
@@ -1,9 +0,0 @@
-var FileClass = (function () {
-    function FileClass(fullpath) {
-        this.fullpath = fullpath;
-    }
-    return FileClass;
-})();
-var file1 = new FileClass('testpath');
-console.log(file1.fullpath);
-//# sourceMappingURL=index.js.map
